9a7cf5d9ce4eae8f97079e886baa35a538dfa78c
Juhi Gupta <juhig@amazon.com>
2021-08-13 20:52:29 +0530

    res and req

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -5,6 +5,16 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send('<h1 style ="color: red;">Hello World</h1>')
 })
+app.get('/greet',(req,res)=>{
+    console.log(req.headers)
+    console.log(req.query)
+    let person = 'guest'
+    if(req.query.person)
+    person = req.query.person
+    res.send('Good Morninggg' + person)
+})
+//http://localhost:4444/greet?a=10
+
 
 app.listen(4444,()=>
 {
