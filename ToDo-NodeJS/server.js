3c799b5b0c0ba1f454490d85576756aa997cd01e
Juhi Gupta <juhig@amazon.com>
2021-08-14 01:59:58 +0530

    TODO List working

--- ToDo-NodeJS/server.js
+++ ToDo-NodeJS/server.js
@@ -7,28 +7,28 @@ let task =[
 app.get('/', (req, res)=>{
     let taskList = task.map(t => `<li>${t}</li>`).join('\n') //take each task and turn it into String <li>${t}</li>'
   console.log(taskList)
-    // BackTick is used in HTML code line 8 and line 11
-//     res.send(` 
-//    <!DOCTYPE html>
-//    <html lang="en">
-//    <head>
-//        <meta charset="UTF-8">
-//        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//        <title>Document</title>
-//    </head>
-//    <body>
-//        <form action="/" method="POST">
-//            <input name = "newTask">
-//            <button type = "submit">ADD</button>
-//        </form>
-//        <ul>
-//           ${taskList}
-//        </ul>
-//    </body>
-//    </html>
-//    `)
-    res.sendFile(__dirname + '/index.html')
+    // BackTick is used in HTML code line 8 and line
+    res.send(` 
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+   </head>
+   <body>
+       <form action="/" method="POST">
+           <input name = "newTask">
+           <button type = "submit">ADD</button>
+       </form>
+       <ul>
+          ${taskList}
+       </ul>
+   </body>
+   </html>
+   `)
+    // res.sendFile(__dirname + '/index.html')
 })
 app.post('/', (req,res)=>{
     console.log("ADD Clicked")
